6d4ace2451856c1d401565b68b475d7d073586d7
CAM1989 <cherepov-a@mail.ru>
2022-05-06 16:11:37 +0300

    HW11

---
+++ lesson9/src/main/java/ru/gb/persist/UserRepository.java
@@ -0,0 +1,13 @@
+package ru.gb.persist;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
+
+    Optional<User> findUserByUsername(@Param("username") String username);
+
+}
